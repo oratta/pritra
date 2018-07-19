b6e219a17f9ce677806bdd88c77f46d0dba5e3df
oratta <oratta@gmail.com>
2018-07-19 17:56:14 +0900

    add first settings

---
+++ database/migrations/2018_07_19_084522_create_trainings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->references('id')->on('users')->unsigned()->index();
+            $table->smallInteger('type');
+            $table->smallInteger('step');
+            $table->smallInteger('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+   'training' => 'TrainingController',
+]);
