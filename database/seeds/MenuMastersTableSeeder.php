5d789f8dc46b093bcd32568ce414f1bd3f1c88b4
oratta <oratta@gmail.com>
2018-10-11 13:02:37 +0900

    menu id fix (not using autoincrement)

--- database/seeds/MenuMastersTableSeeder.php
+++ database/seeds/MenuMastersTableSeeder.php
@@ -13,26 +13,32 @@ class MenuMastersTableSeeder extends Seeder
     {
         DB::table('menu_masters')->insert([
             [
+                'id'    => 1,
                 'name'  => 'プッシュアップ',
                 'description' => '',
             ],
             [
+                'id'    => 2,
                 'name'  => 'スクワット',
                 'description' => '',
             ],
             [
+                'id'    => 3,
                 'name'  => 'プルアップ',
                 'description' => '',
             ],
             [
+                'id'    => 4,
                 'name'  => 'レッグレイズ',
                 'description' => '',
             ],
             [
+                'id'    => 5,
                 'name'  => 'ブリッジ',
                 'description' => '',
             ],
             [
+                'id'    => 6,
                 'name'  => 'ハンドスタンドプッシュアップ',
                 'description' => '',
             ],
