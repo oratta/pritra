335c76c48b6114322e9358efb7726cd10b81727b
oratta <oratta@gmail.com>
2018-10-12 18:26:19 +0900

    delete comment
    that from sample code

--- resources/views/workout/create.blade.php
+++ resources/views/workout/create.blade.php
@@ -79,27 +79,22 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript">
-            var $children = $('.children'); //都道府県の要素を変数に入れます。
-            var original = $children.html(); //後のイベントで、不要なoption要素を削除するため、オリジナルをとっておく
+            var $children = $('.children');
+            var original = $children.html();
 
-            //地方側のselect要素が変更になるとイベントが発生
             $('.parent').change(function() {
 
-                //選択された地方のvalueを取得し変数に入れる
                 var val1 = $(this).val();
 
-                //削除された要素をもとに戻すため.html(original)を入れておく
                 $children.html(original).find('option').each(function() {
                     var val2 = $(this).data('val'); //data-valの値を取得
 
-                    //valueと異なるdata-valを持つ要素を削除
                     if (val1 != val2) {
                         $(this).not(':first-child').remove();
                     }
 
                 });
 
-                //地方側のselect要素が未選択の場合、都道府県をdisabledにする
                 if ($(this).val() == "") {
                     $children.attr('disabled', 'disabled');
                 } else {
