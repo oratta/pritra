be398e56deae32193c094336c20113bf2b26f5b8
oratta <oratta@gmail.com>
2019-06-03 17:56:28 +0900

    add backup command

--- app/Console/Commands/CopyProductDb.php
+++ app/Console/Commands/CopyProductDb.php
@@ -48,12 +48,32 @@ class CopyProductDb extends Command
         $productDbPassword = env("PRODUCT_DB_PASSWORD");
         $dumpCommand = "sudo docker-compose exec --user=$productDbContainerUser $productDbContainer /usr/bin/mysqldump -u $productDbUser --password=$productDbPassword $productDbName";
         $productServerCommand = '"cd laradock;' . $dumpCommand . ';"';
-        $sshCommand = "ssh $productHost -l oratta $productServerCommand";
+        $date = date('Ymd_His');
+        $outputFile = "/tmp/dump.$date.sql";
+        $outputCommand = " > $outputFile";
+        $sshCommand = "ssh $productHost -l oratta $productServerCommand $outputCommand";
         $return = exec($sshCommand, $arr, $arr2);
         dump($sshCommand);
         dump($return);
         dump($arr);
         dump($arr2);
+
+        //upload to dropbox
+
+        $dropboxToken = env("PRODUCT_BACKUP_DROPBOX_TOKEN");
+        $backupPath = env("PRODUCT_BACKUP_DROPBOX_PATH") . "dump.$date.sql";
+        $uploadCommand = 'curl -X POST https://content.dropboxapi.com/2/files/upload \
+    --header "Authorization: Bearer ' . $dropboxToken .'" \
+    --header "Dropbox-API-Arg: {\"path\": \"'.$backupPath. '\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary @' . $outputFile;
+
+        $returnUpdate = exec($uploadCommand, $arr, $arr2);
+        dump($uploadCommand);
+        dump($return);
+        dump($arr);
+        dump($arr2);
+
         //drop db if force option
         //if same name db exist
         //if it can be contain a drop db command in a dump file, this operation is not necessary
