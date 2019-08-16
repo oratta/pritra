598fcac9cea212e94e4369046c200de81b07ad0b
oratta <oratta@gmail.com>
2019-08-16 14:56:42 +0900

    artisanからdeployerの呼び出し

---
+++ app/Console/Commands/Deploy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Deploy extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'deploy:all
+                            {stage}
+    ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'deploy all';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //file copy (master branch and .env)
+        $stage = $this->argument('stage');
+        $command = "./vendor/bin/dep deploy $stage";
+        dump('exec' . $command);
+        $return = exec($command, $arr, $arr2);
+        dump($return);
+        dump($arr);
+        dump($arr2);
+
+        //composer install(option)
+
+        //artisan:storage:link
+
+        //artisan:view:clear
+
+        //artisan:cache:clear
+
+        //artisan:config:cache
+
+        //artisan:optimize
+
+        //migrate (option)
+
+    }
+}
