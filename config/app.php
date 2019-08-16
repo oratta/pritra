7b0f6a232e5bd64fef5a07175f4180cf75101373
oratta <oratta@gmail.com>
2019-08-16 17:53:17 +0900

    symfony/yamlのインストール

--- config/app.php
+++ config/app.php
@@ -226,6 +226,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Yaml' => 'Symfony\Component\Yaml\Yaml',
     ],
 
 ];
