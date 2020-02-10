a3e4748538d2c1394f8f15b60097fa86e2470fd4
oratta <oratta@gmail.com>
2020-02-10 17:27:35 +0900

    toggle button and move menu cards to component

--- resources/assets/js/components/MenuCard.vue
+++ resources/assets/js/components/MenuCard.vue
@@ -1,40 +1,212 @@
 <template>
-    <v-card>
+    <v-card class="menu-card">
         <v-card-subtitle>
             Push Up
         </v-card-subtitle>
         <v-img
                 src="https://hercules-beetle.com/wp-content/uploads/2017/03/full-pushup-01.png"
-                width="100%"></v-img>
+                height="200px"></v-img>
         <v-card-title>
-                Full Pull Up
+            Full Pull Up
         </v-card-title>
-        <v-row>
-            <v-col
-                    cols="8"
-            >
-                <v-card-text>20reps × 2set</v-card-text>
-            </v-col>
-            <v-col
-                    cols="4"
-            >
-                <v-card-actions>
-                    <v-row>
-                        <v-col>
-                            <v-btn icon>Up</v-btn>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col>
-                            <v-btn icon>Down</v-btn>
-                        </v-col>
-                    </v-row>
-                </v-card-actions>
-            </v-col>
-        </v-row>
-        <v-card-actions>
-            <v-btn>History</v-btn>
-            <v-btn>Set</v-btn>
-        </v-card-actions>
+        <form>
+            <div class="menu-card__form_training_set">
+                <div class="menu-card__form_training_set__input">
+                    <div class="count_box">
+                        <div class="count">
+                            20
+                        </div>
+                        <div class="count_label">
+                            reps
+                        </div>
+                    </div>
+                    <div class="button_up_down">
+                        <div class="button_up_down__up">
+                            <v-btn icon small>
+                                <v-icon small>mdi-arrow-up-bold</v-icon>
+                            </v-btn>
+                        </div>
+                        <div class="button_up_down__down">
+                            <v-btn icon small>
+                                <v-icon small>mdi-arrow-down-bold</v-icon>
+                            </v-btn>
+                        </div>
+                    </div>
+                    <div class="menu-card__form_training_set__input__spacer"></div>
+                    <div class="count_box">
+                        <div class="count">
+                            2
+                        </div>
+                        <div class="count_label">
+                            set
+                        </div>
+                    </div>
+                    <div class="button_up_down">
+                        <div class="button_up_down__up">
+                            <v-btn icon small>
+                                <v-icon small>mdi-arrow-up-bold</v-icon>
+                            </v-btn>
+                        </div>
+                        <div class="button_up_down__down">
+                            <v-btn icon small>
+                                <v-icon small>mdi-arrow-down-bold</v-icon>
+                            </v-btn>
+                        </div>
+                    </div>
+                </div>
+                <div class="menu-card__form_training_set__submit">
+                    <v-card-actions>
+                        <v-btn
+                                size="small"
+                                @click="isInfoBox=!isInfoBox"
+                        >
+                            Info
+                        </v-btn>
+                    </v-card-actions>
+                    <div class="spacer"></div>
+                    <v-card-actions>
+                        <v-btn
+                                color="accent"
+                                size="small"
+                        >
+                            Set
+                        </v-btn>
+                    </v-card-actions>
+                </div>
+            </div>
+        </form>
+        <div class="menu-card__info_box" v-show="isInfoBox">
+            <div class="level_box">
+                <div class="level_box__title">
+                    <v-icon>mdi-run</v-icon>Push Up
+                </div>
+                <div class="level_box__card">
+                    <div class="level_box__card__step_name">1:FullPush</div>
+                    <ul>
+                        <li>
+                            Lv1 : 10 × 2
+                        </li>
+                        <li>
+                            Lv2 : 15 × 2
+                        </li>
+                        <li>
+                            Lv3 : 20 × 3
+                        </li>
+                    </ul>
+                </div>
+                <div class="level_box__card">
+                    <div class="level_box__card__step_name">1:FullPush</div>
+                    <ul>
+                        <li>
+                            Lv1 : 10 × 2
+                        </li>
+                        <li>
+                            Lv2 : 15 × 2
+                        </li>
+                        <li>
+                            Lv3 : 20 × 3
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="level_box__card">
+                    <div class="level_box__card__step_name">1:FullPush</div>
+                    <ul>
+                        <li>
+                            Lv1 : 10 × 2
+                        </li>
+                        <li>
+                            Lv2 : 15 × 2
+                        </li>
+                        <li>
+                            Lv3 : 20 × 3
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="level_box__card">
+                    <div class="level_box__card__step_name">1:FullPush</div>
+                    <ul>
+                        <li>
+                            Lv1 : 10 × 2
+                        </li>
+                        <li>
+                            Lv2 : 15 × 2
+                        </li>
+                        <li>
+                            Lv3 : 20 × 3
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="level_box__card">
+                    <div class="level_box__card__step_name">1:FullPush</div>
+                    <ul>
+                        <li>
+                            Lv1 : 10 × 2
+                        </li>
+                        <li>
+                            Lv2 : 15 × 2
+                        </li>
+                        <li>
+                            Lv3 : 20 × 3
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="level_box__card">
+                    <div class="level_box__card__step_name">1:FullPush</div>
+                    <ul>
+                        <li>
+                            Lv1 : 10 × 2
+                        </li>
+                        <li>
+                            Lv2 : 15 × 2
+                        </li>
+                        <li>
+                            Lv3 : 20 × 3
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="history_box">
+                <div class="history_box__title">
+                    <v-icon>mdi-panda</v-icon>Best
+                </div>
+                <div class="history_box__card">
+                    <div class="history_box__card__step_name">Step1 FullPush</div>
+                    <div class="history_box__card__count">15 × 2</div>
+                    <div class="history_box__card__time">2019/02/10 9:24</div>
+                </div>
+                <div class="history_box__title">
+                    <v-icon>mdi-history</v-icon>Recent
+                </div>
+                <div class="history_box__card">
+                    <div class="history_box__card__step_name">Step1 FullPush</div>
+                    <div class="history_box__card__count">15 × 2</div>
+                    <div class="history_box__card__time">2019/02/10 9:24</div>
+                </div>
+                <div class="history_box__card">
+                    <div class="history_box__card__step_name">Step1 FullPush</div>
+                    <div class="history_box__card__count">15 × 2</div>
+                    <div class="history_box__card__time">2019/02/10 9:24</div>
+                </div>
+                <div class="history_box__card">
+                    <div class="history_box__card__step_name">Step1 FullPush</div>
+                    <div class="history_box__card__count">15 × 2</div>
+                    <div class="history_box__card__time">2019/02/10 9:24</div>
+                </div>
+            </div>
+        </div>
     </v-card>
 </template>
+<script>
+export default{
+    data(){
+        return {
+            isInfoBox: false,
+        }
+    },
+}
+</script>
