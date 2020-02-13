eee7a3e76fc211e0e52637bfa17b403f43e95d2b
oratta <oratta@gmail.com>
2020-02-13 17:50:02 +0900

    add card pushing set buttons

--- resources/assets/js/pages/SetMenu.vue
+++ resources/assets/js/pages/SetMenu.vue
@@ -8,25 +8,45 @@
                 ></MenuCard>
             </v-col>
             <v-col lg="4" md="4" sm="6" cols="12" >
-                <MenuCard></MenuCard>
+                <MenuCard
+                        v-on:set-menu="setMenu"
+                ></MenuCard>
             </v-col>
             <v-col lg="4" md="4" sm="6" cols="12" >
-                <MenuCard></MenuCard>
+                <MenuCard
+                        v-on:set-menu="setMenu"
+                ></MenuCard>
             </v-col>
         </v-row>
         <v-row>
             <v-col lg="4" md="4" sm="6" cols="12" >
-                <MenuCard></MenuCard>
+                <MenuCard
+                        v-on:set-menu="setMenu"
+                ></MenuCard>
             </v-col>
             <v-col lg="4" md="4" sm="6" cols="12" >
-                <MenuCard></MenuCard>
+                <MenuCard
+                        v-on:set-menu="setMenu"
+                ></MenuCard>
             </v-col>
             <v-col lg="4" md="4" sm="6" cols="12" >
-                <MenuCard></MenuCard>
+                <MenuCard
+                        v-on:set-menu="setMenu"
+                ></MenuCard>
             </v-col>
         </v-row>
         <div class="cart" v-show="isShowCart">
             ここにカート用のフッターが表示されるよ
+            <div class="mini_menu_card" v-for="miniCard in miniCardInfo">
+                <v-card>
+                    <v-card-title>PushUp</v-card-title>
+                    <v-card-text>Full Push Up</v-card-text>
+                    <v-card-text>20×2</v-card-text>
+                    <div class="remove_btn">
+                        <v-btn small>Remove</v-btn>
+                    </div>
+                </v-card>
+            </div>
             <div class="training_start_btn">
                 <v-btn color="primary">Start</v-btn>
             </div>
@@ -42,12 +62,17 @@ export default {
     },
     data(){
         return {
-          isShowCart: false,
+            isShowCart: false,
+            miniCardInfo: [1,2],
         }
     },
     methods:{
         setMenu: function(){
             this.isShowCart = !this.isShowCart;
+            this.addMiniCard();
+        },
+        addMiniCard: function(){
+            this.miniCardInfo.push(3);
         }
     }
 }
