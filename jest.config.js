7f938cd3305e64dbc6915b150925834d6d7c2513
oratta <oratta@gmail.com>
2020-04-21 21:36:31 +0900

    テスト環境構築

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  testMatch: ['**/tests/js/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)']
 }
