07a95e00ededb5b5a803276a95258a9c5e41e153
oratta <oratta@gmail.com>
2020-05-12 11:32:34 +0900

    multi work set

--- resources/assets/js/components/MenuCardRun.vue
+++ resources/assets/js/components/MenuCardRun.vue
@@ -31,14 +31,17 @@
             </div>
         </div>
         <div class="menu-card__form_training_set">
-            <div class="menu-card__form_training_set__input">
+            <div
+                    class="menu-card__form_training_set__input"
+                    v-for="workout in workout_s"
+            >
                 <div class="set_number_label">
-                    1st
+                    {{workout.label}}
                 </div>
                 <div class="menu-card__form_training_set__input__spacer"></div>
                 <div class="count_box">
                     <div class="count">
-                        {{selectedWorkoutSet.rep}}
+                        {{workout.rep}}
                     </div>
                     <div class="count_label">
                         reps
@@ -46,12 +49,12 @@
                 </div>
                 <div class="button_up_down">
                     <div class="button_up_down__up">
-                        <v-btn icon small @click="selectedWorkoutSet.rep +=1">
+                        <v-btn icon small @click="workout.rep +=1">
                             <v-icon small>mdi-arrow-up-bold</v-icon>
                         </v-btn>
                     </div>
                     <div class="button_up_down__down">
-                        <v-btn icon small @click="selectedWorkoutSet.rep -=1">
+                        <v-btn icon small @click="workout.rep -=1">
                             <v-icon small>mdi-arrow-down-bold</v-icon>
                         </v-btn>
                     </div>
@@ -65,9 +68,24 @@
                             label="training load..."
                     ></v-select>
                 </div>
+                <div class="menu-card__form_training_set__input__spacer"></div>
+                <div class="fin_button">
+                    <v-btn
+                            :disabled="workout.isFinish"
+                            color="accent"
+                            size="small"
+                            @click="workout.isFinish=true"
+                    >
+                        Fin
+                    </v-btn>
+                </div>
             </div>
             <div class="add_set_button center">
-                <v-btn class="mx-2" fab dark color="indigo">
+                <v-btn
+                        class="mx-2"
+                        fab dark color="indigo"
+                        @click="addWorkout"
+                >
                     <v-icon dark>mdi-plus</v-icon>
                 </v-btn>
             </div>
@@ -92,13 +110,20 @@
 export default{
     data(){
         return {
-            selectedWorkoutSet: {
+            objectiveWorkoutSet: {
                 rep: 20,
+                set: 2,
             },
             sampleValue: 0,
             sampleItems: [
                 'banana', 'apple', 'orange'
             ],
+            workout_s:[],
+            SET_NAME_LABEL: {
+                1: '1st',
+                2: '2nd',
+                3: '3rd',
+            }
         }
     },
     props: {
@@ -113,6 +138,23 @@ export default{
     },
     methods: {
         init: function(){
+            this.workout_s.push({rep:20, label:"1st", isFinish:false});
+        },
+        addWorkout: function(){
+            let label = this.getSetNameLabel(this.workout_s.length + 1)
+            this.workout_s.push({
+                rep: this.objectiveWorkoutSet.rep,
+                label: label,
+                isFinish:false,
+            });
+        },
+        getSetNameLabel: function(number){
+            if(number<4){
+                return this.SET_NAME_LABEL[number];
+            }
+            else{
+                return number + "th";
+            }
         },
     },
     created () {
