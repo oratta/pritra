e197bffc52bb05e10de8d6b2da5e5af071b1f8e6
oratta <oratta@gmail.com>
2020-06-04 11:16:57 +0900

    log settings
    it is for output stack traces of logger. but it dosen't work

--- config/logging.php
+++ config/logging.php
@@ -43,6 +43,7 @@ return [
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
+            'tap' => [App\Logging\CustomizeFormatter::class],
             'level' => 'debug',
         ],
 
