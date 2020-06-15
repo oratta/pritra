26f84d7e4afac3613f1f68837d9c115e94d32946
oratta <oratta@gmail.com>
2020-06-15 15:56:01 +0900

    delete not using module

--- app/Http/Controllers/WorkoutSetController.php
+++ app/Http/Controllers/WorkoutSetController.php
@@ -6,7 +6,6 @@ use App\Exceptions\BadRequestException;
 use App\Model\Master\MenuMaster;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use App\Model\UserData\WorkoutSet;
 use Illuminate\Database\Eloquent\Collection;
 
