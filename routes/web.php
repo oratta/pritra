2d3f50008fb7757b9dc5791a7c051acb6a84bfa3
oratta <oratta@gmail.com>
2020-07-17 11:11:21 +0900

    旧バージョンのビューを削除

--- routes/web.php
+++ routes/web.php
@@ -11,69 +11,8 @@
 |
 */
 
-/*
-Route::get('/{any}', function () {
-//    return view('workout.app');
-    return view('sample.vue');
-})->where('any','.*');
-*/
-
-//Route::get('sample/vue2', function (){
-//    return view('sample.vue2');
-//});
-
 // APIのURL以外のリクエストに対してはindexテンプレートを返す
 // 画面遷移はフロントエンドのVueRouterが制御する
 Route::get('/{any?}', function () {
     return view('index');
 })->where('any', '.+');
-
-
-Route::get('/', 'WorkoutController@index');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/', 'WorkoutController@index');
-
-    Route::prefix('workout')->group(function () {
-        Route::get('/', 'WorkoutController@index');
-        Route::get('index', 'WorkoutController@index');
-
-        Route::get('create/{recomend?}', 'WorkoutController@create');
-        Route::post('store', 'WorkoutController@store');
-    });
-
-    Route::prefix('menus')->group(function () {
-        Route::get('/', 'MenuController@index');
-        Route::get('index', 'MenuController@index');
-        Route::get('show/{id}', 'MenuController@show');
-    });
-
-    Route::prefix('steps')->group(function () {
-        Route::get('/', 'StepController@index');
-        Route::get('index', 'StepController@index');
-        Route::get('show/{id}', 'StepController@show');
-    });
-
-    Route::prefix('sample')->group(function () {
-        Route::get('/', 'SampleController@index');
-        Route::get('index', 'SampleController@index');
-        Route::view('tab', 'sample.tab');
-    });
-
-
-});
-
-Route::middleware(['adminCheck'])->group(function(){
-    Route::prefix('admin')->group(function(){
-        Route::get('index', 'AdminController@index');
-        Route::get('/', 'AdminController@index');
-        Route::post('add_user', 'AdminController@addUser');
-    });
-});
-
-
-
-//Auth::routes();
-
-//Route::get('/home', 'HomeController@index')->name('home');
-//Route::get('test/firebase', 'FirebaseTestController@tests');
