d928221198c891bf0675857543a82a7ae7493334
oratta <oratta@gmail.com>
2020-07-28 15:58:45 +0900

    [WIP] apply design
    ・copy sass file
    ・set up webpack
    https://vuetifyjs.com/ja/getting-started/quick-start/#webpack-install

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,18 +3,16 @@ import Vue from 'vue'
 import router from './router'
 import store from './store'
 import App from './App.vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify'
 import './plugins/dashboard'
 import './plugins/chartist'
-Vue.use(Vuetify)
 
 const createApp = async () =>{
     await store.dispatch('auth/currentUser')
 
     new Vue({
         el: '#app',
-        vuetify: new Vuetify(),
+        vuetify,
         router,
         store,
         components: {App},
